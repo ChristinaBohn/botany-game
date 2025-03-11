1649dbe8a8a1061628cb452f37d2f8c3628a2cb4
Christina Bohn <christinajbohn@gmail.com>
2025-03-11 11:49:02 -0700

    Begin work on game functions

--- assets/script.js
+++ assets/script.js
@@ -26,6 +26,7 @@ const xpText = document.querySelector('#xpText');
 const coinText = document.querySelector('#coinText');
 
 // Plant stats
+const plantTiles = document.querySelector('#plantTiles');
 const plantName = document.querySelector('#plantName');
 const healthText = document.querySelector('#healthText');
 
@@ -63,13 +64,13 @@ const locations = [
     {
         name: "light",
         "button text": ["Place plant in low light", "Place plant in medium light", "Place plant in bright light"],
-        "button functions": [addPlant, addPlant, addPlant],
+        "button functions": [loseGame, askWater, askWater],
         text: "Where on your shelf will you put your new plant?"
     },
     {
         name: "water",
         "button text": ["Don't water at all", "Water a little", "Water a lot"],
-        "button functions": [addPlant, addPlant, addPlant],
+        "button functions": [addPlant, addPlant, loseGame],
         text: "How much water do you want to give your plant?"
     },
     {
@@ -77,12 +78,54 @@ const locations = [
     },
     {
         name: "kill plant"
+    },
+    {
+        name: "lose game",
+        "button text": ["Start over?", "Start over?", "Start over?"],
+        "button functions": [restart, restart, restart],
+        text: "Oh no, your new plant didn't like that! Game over. Would you like to play again?"
     }
     // later on could add "plant shop"
 ];
 
 
 // Initialize buttons
-button1.onclick = addPlant;
-button2.onclick = addPlant;
-button3.onclick = addPlant;
+button1.onclick = loseGame;
+button2.onclick = askWater;
+button3.onclick = askWater;
+
+function update(location) {
+
+    // hides the plant tiles
+    plantTiles.style.display = "none";
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerHTML = location.text;
+}
+
+function askLight() {
+    update(locations[3]);
+}
+
+function askWater() {
+
+};
+
+function addPlant() {};
+
+function loseGame() {
+    // Lose game message and restart
+    update(locations[7]);
+};
+
+function restart() {
+    xp = 0;
+    coins = 50;
+    xpText.innerText = xp;
+    coinsText.innerText = coins;
+    askLight();
+}
