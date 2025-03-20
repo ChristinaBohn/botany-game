acf891415b25a1a51d40932c8fc8008673351435
Christina Bohn <christinajbohn@gmail.com>
2025-03-20 11:55:37 -0700

    Begin work on expanding game to array of random plants to be added to collection

--- README.md
+++ README.md
@@ -1,20 +1,4 @@
 # botany-game
 An interactive plant care game written with HTML, CSS, and Javascript. Try out your green thumb without killing real plants at home!
 
-## game planning
-
-1. you begin with an easy care/level 1 plant (snake plant), you are given 3 choices of light to place it in. All three options are correct for level 1 plant, so plant is added to your collection
-
-2. you are then promped to water the plant a lot, a little, or not at all. Again, all three are correct for a level 1
-
-3. so you next recieve a level 2 plant. Again options for light, this time only med and bright lights are acceptable, if correct, add plant to collection and move on to water question. if false, then you kill the plant and start over receiving another level 1 plant.
-
-4. once you add a level 2 plant successfully to your collection, you receive a level 3 plant. Options for light (most strict, only medium acceptable) then options for water (only a little is acceptable). if false, go back to level 1 plant. if all correct, add plant to collection and say you won!
-
-5. Later add: do you want to continue playing? then options to visit shelf, start back at level 1, or reset and go back to level 1
-
-** every time you receive a plant, water is subtracted by 1 from all plants in collection (time passing)
-
-** at any point you can visit plant shelf and: water plants (too much will kill plant, back to start receiving plants) or sell clippings (subtract from plant health, add to coins) then button for back to home
-
-** if any plant water level lowers to lowest level in range, alert player that the plant is wilting, direct to visit plant shelf button
+## Visit Game >> https://christinabohn.github.io/botany-game/
--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,6 @@
-// Each plant is an object, it will have name, water(health), light (first get right or wrong, then move on), clipping cost, do not combine water and light to make health, just keep separate
-
 let xp = 0;
 let coins = 30;
 let plantHealth = 100;
-let currentLight;
-let currentPlant;
 let collection = [];
 
 // Player controls
@@ -18,84 +14,98 @@ const button5 = document.querySelector('#button5');
 
 // Text
 const text = document.querySelector('#text');
-
-// Player stats
 const xpText = document.querySelector('#xpText');
 const coinText = document.querySelector('#coinText');
 
-// Plant stats
+// Plant collection
 const plantTiles = document.querySelector('#plantTiles');
 const plantName = document.querySelector('#plantName');
 const healthText = document.querySelector('#healthText');
 
 const plants = [
     {
-        name: "Snake Plant",
-        level: 1,
-        light: ["low", "medium", "bright"],
-        water: 1 - 10,
+        id: 0,
+        name: "Snake Plant (easy care)",
+        light: {
+            "button text": ["Place plant in low light", "Place plant in medium light", "Place plant in bright light"],
+            "button functions": [askWater, askWater, askWater],
+            text: "You have received a Snake Plant (easy care)! Where on your plant shelf will you place your plant?"
+        },
+        water: {
+            "button text": ["Don't water at all", "Water a little", "Water a lot"],
+            "button functions": [addPlant, addPlant, addPlant],
+            text: "Good job! Snake plants are happy in any light. How much water do you want to give your plant?"
+        },
         clippingCost: 5
     },
     {
-        name: "Hoya",
-        level: 2,
-        light: ["medium", "bright"],
-        water: 3 - 8,
+        id: 1,
+        name: "Hoya (medium care)",
+        light: {
+            "button text": ["Place plant in low light", "Place plant in medium light", "Place plant in bright light"],
+            "button functions": [loseGame, askWater, askWater],
+            text: "You have received a Hoya (medium care)! Where on your plant shelf will you place your plant?"
+        },
+        water: {
+            "button text": ["Don't water at all", "Water a little", "Water a lot"],
+            "button functions": [loseGame, addPlant, loseGame],
+            text: "Good job! Hoyas are happy in medium to bright light. How much water do you want to give your plant?"
+        },
         clippingCost: 10
     },
     {
-        name: "Alocasia",
-        level: 3,
-        light: ["medium"],
-        water: 4 - 6,
+        id: 2,
+        name: "Calathea (difficult care)",
+        light: {
+            "button text": ["Place plant in low light", "Place plant in medium light", "Place plant in bright light"],
+            "button functions": [loseGame, askWater, loseGame],
+            text: "You have received a Clathea (difficult care)! Where on your plant shelf will you place your plant?"
+        },
+        water: {
+            "button text": ["Don't water at all", "Water a little", "Water a lot"],
+            "button functions": [loseGame, addPlant, loseGame],
+            text: "Good job! Calatheas are happy in medium light only. How much water do you want to give your plant?"
+        },
         clippingCost: 15
     }
 ];
 
 const locations = [
     {
-        name: "home"
+        name: "welcome",
+        "button text": ["Begin!", "Begin!", "Begin!"],
+        "button functions": [newPlant, newPlant, newPlant],
+        text: "Welcome to Botany Bliss. Take care of each new plant based on your plant knowledge and watch your plant collection grow!"
     },
     {
-        name: "shelf"
-    },
-    {
-        name: "light",
-        "button text": ["Place plant in low light", "Place plant in medium light", "Place plant in bright light"],
-        "button functions": [loseGame, askWater, askWater],
-        text: "Welcome to Botany Bliss! You've just been gifted a Hoya (care level: medium). You bring the plant home, what is your next move?"
-    },
-    {
-        name: "water",
-        "button text": ["Don't water at all", "Water a little", "Water a lot"],
-        "button functions": [addPlant, addPlant, loseGame],
-        text: "Good job! Hoyas are happy in medium to bright light. How much water do you want to give your plant?"
-    },
-    {
-        name: "add plant"
-    },
-    {
-        name: "kill plant"
+        name: "lose plant",
+        "button text": ["Try again", "Try again", "Try again"],
+        "button functions": [newPlant, newPlant, newPlant],
+        text: "Oh no, your new plant didn't like that! You've lost this plant. Try again?"
     },
     {
         name: "lose game",
         "button text": ["Start over?", "Start over?", "Start over?"],
-        "button functions": [restart, restart, restart],
-        text: "Oh no, your new plant didn't like that! Game over. Would you like to play again?"
+        "button functions": [welcome, welcome, welcome],
+        text: "Oh no, your new plant didn't like that and you have no remaining plants in your collection! Game over. Would you like to play again?"
+    },
+    {
+        name: "win game",
+        "button text": ["Start over?", "Start over?", "Start over?"],
+        "button functions": [welcome, welcome, welcome],
+        text: "Congratulations, you have every available plant in your home collection! Look at those green thumbs. Would you like to play again?"
     }
-    // later on could add "plant shop"
 ];
 
+let plantShop = [...plants];
 
 // Initialize buttons
-button1.onclick = loseGame;
-button2.onclick = askWater;
-button3.onclick = askWater;
+button1.onclick = newPlant;
+button2.onclick = newPlant;
+button3.onclick = newPlant;
+
 
 function update(location) {
-
-    // hides the plant tiles
-    plantTiles.style.display = "none";
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
@@ -105,9 +115,42 @@ function update(location) {
     text.innerHTML = location.text;
 }
 
-function askLight() {
-    update(locations[2]);
+function welcome() {
+    xp = 0;
+    coins = 50;
+    xpText.innerText = xp;
+    coinText.innerText = coins;
+    collection = [""];
+    plantShop = [...plants];
+    update(locations[0]);
+    newPlant();
 }
+
+// console.log(plants[0].light.text);
+// console.log(plants[1]["light"]["button text"][0]);
+
+// Receive random plant and ask about light
+function newPlant() {
+    let plantIndex = Math.floor(Math.random() * 3);
+    let currentPlant = plants[plantIndex];
+
+    button1.innerText = currentPlant[light]["button text"][0];
+    button2.innerText = currentPlant[light]["button text"][1];
+    button3.innerText = currentPlant[light]["button text"][2];
+    button1.onclick = currentPlant[light]["button functions"][0];
+    button2.onclick = currentPlant[light]["button functions"][1];
+    button3.onclick = currentPlant[light]["button functions"][2];
+
+    console.log(plantId);
+    console.log(currentPlant.light.text);
+    text.innerHTML = currentPlant.light.text;
+
+
+}
+
+// function askLight() {
+//     update(locations[2]);
+// }
 
 function askWater() {
     update(locations[3]);
@@ -126,10 +169,32 @@ function loseGame() {
     xpText.innerText = xp;
 };
 
-function restart() {
-    xp = 0;
-    coins = 50;
-    xpText.innerText = xp;
-    coinText.innerText = coins;
-    askLight();
-}
+welcome();
+
+// Game planning
+
+// Welcome (restart takes you here, or losing all plants)
+    // Explain game and reveal begin button - takes you to new plant
+// newPlant
+    // currentPlant = random plant from plantShop array
+    // text: you have recieved a ${new plant name}
+        // ask light
+            // if win { move on to askWater }
+            // if lose { plant stays in shop and you can restart }
+        // ask water
+            // if win { 
+                // remove from shop and add to collection
+                // xp ++
+                // reveal text that says congrats! added to collection. Are you ready for a new plant? Begin button > newPlant 
+            // }
+            // if lose { plant stays in shop and you can restart }
+
+// Display plant shop with price, when plant dies but you still have coins and a plant in your collection
+// Display option to buy plant
+
+// Reveal plant tiles
+// plantTiles.style.display = "block";
+
+// ** every time you receive a plant, water is subtracted by 1 from all plants in collection (time passing)
+// ** at any point you can visit plant shelf and: water plants (too much will kill plant, back to start receiving plants) or sell clippings (subtract from plant health, add to coins) then button for back to home
+// ** if any plant water level lowers to lowest level in range, alert player that the plant is wilting, direct to visit plant shelf button
